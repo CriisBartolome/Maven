44d7b735d745baf81bd9defd2c29370a5b39c6d4
cbartolome <cristina.bartolome@soprasteria.com>
2021-04-13 12:18:26 +0200

    Ejemplo de site (documentacion)

---
+++ Ejemplo2/src/main/java/com/soprasteria/Ejemplo2/Prueba.java
@@ -0,0 +1,14 @@
+package com.soprasteria.Ejemplo2;
+
+/**
+ * Mi descripcion
+ * 
+ * @author cbartolome
+ * @version 1
+ */
+public class Prueba {
+
+	public String getString() {
+		return "Hola mundo";
+	}
+}
